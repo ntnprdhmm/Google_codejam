fc72d2703ba80ed24972e5418df066bf009a1a83
prudywsh <antoineprudhomme5@gmail.com>
2018-03-25 18:53:04 +0200

    Qualification round 2017: bathroom stalls (large dataset ok)

--- Qualification_Round_2017/bathroom_stalls.py
+++ Qualification_Round_2017/bathroom_stalls.py
@@ -7,21 +7,25 @@ f_out = open(f_name + ".out", "w")
 def solve(n, k):
     if n == k: return (0, 0)
 
-    q = []
-    heappush(q, -n)
-    for _ in range(k-1):
-        b = -heappop(q) # pop the biggest range of free stalls
-        left = (b-1) // 2
-        right = b // 2
-        #print("%d %d" % (left, right))
-        heappush(q, -right)
-        heappush(q, -left)
+    p_queue = [-n] # priority queue => biggest stalls ranges first
+    gaps_sizes = {n: 1} # remember the number of gaps of the same size
 
-    b = -heappop(q)
-    left = b // 2
-    right = (b-1) // 2
-    #print("%d %d" % (left, right))
-    return left, right
+    while True:
+        biggest = -heappop(p_queue) # pop the biggest range of free stalls
+        nb_gaps = gaps_sizes[biggest] # number of gaps with the same size
+        del gaps_sizes[biggest] # remove this entry from dict (useless to keep it)
+        if k <= nb_gaps:
+            # the last person will take one of this gap
+            return biggest // 2, (biggest-1) // 2
+        else:
+            k -= nb_gaps
+            left, right = (biggest-1) // 2, biggest // 2
+            for gap in [left, right]:
+                # if this gap doesn't exists => create it
+                if not gap in gaps_sizes:
+                    gaps_sizes[gap] = 0
+                    heappush(p_queue, -gap)
+                gaps_sizes[gap] += nb_gaps
 
 for test in range(int(f_in.readline().strip())):
     n, k = map(int, f_in.readline().split())
